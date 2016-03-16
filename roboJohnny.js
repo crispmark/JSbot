d421e3f29c7c3161cf5ca4c36ccd172493071e7d
Phil Rajchgot <tophil@outlook.com>
2016-03-16 18:36:05 -0400

    adding front left LED

--- roboJohnny.js
+++ roboJohnny.js
@@ -8,7 +8,7 @@ const board = new five.Board({
 // pull motor configs from ADAFRUIT_V2 template
 const configs = five.Motor.SHIELD_CONFIGS.ADAFRUIT_V2;
 
-// instantiate motors when board is ready
+// instantiate motors and LEDs when board is ready
 var m1;
 var m2;
 var backLed;
@@ -18,9 +18,14 @@ var rightLed;
 board.on('ready', function() {
   m1 = new five.Motor(configs.M1);
   m2 = new five.Motor(configs.M2);
+
   backLed = new five.Led('GPIO21');
+  leftLed = new five.Led('GPIO17');
   rightLed = new five.Led('GPIO4');
+
+  // initial state of LEDs should be 'on'
   backLed.on();
+  leftLed.on();
   rightLed.on();
 });
 
@@ -80,39 +85,51 @@ function customSpeed(dx, dy) {
   m2.fwd(Math.abs(m2speed));
   else m2.rev(m2speed);
 }
+
 // SPEED_MOD compensates for tendency to drift to one side over time
 function forward (speed) {
   m1.fwd(speed);
   m2.fwd(speed * SPEED_MOD);
+
+  // if lights are blinking, stop them and set them to 'on'
   backLed.stop().on();
+  leftLed.stop().on();
   rightLed.stop().on();
 }
 
 function reverse (speed) {
   m1.rev(speed);
   m2.rev(speed * SPEED_MOD);
+
   backLed.blink(150);
+  leftLed.stop().on();
   rightLed.stop().on();
 }
 
 function left (speed) {
   m1.fwd(speed);
   m2.rev(speed * SPEED_MOD);
+
   backLed.stop().on();
+  leftLed.blink(150);
   rightLed.stop().on();
 }
 
 function right (speed) {
   m1.rev(speed);
   m2.fwd(speed * SPEED_MOD);
+
   backLed.stop().on();
+  leftLed.stop().on();
   rightLed.blink(150)
 }
 
 function stop() {
   m1.stop();
   m2.stop();
+
   backLed.stop().on();
+  leftLed.stop().on();
   rightLed.stop().on();
 }
 
