d62ee07ae6d911b4d88396f8fcb0aa72f4728cfe
crispmark <macr1324@gmail.com>
2016-03-16 19:48:17 -0400

    angle tweak

--- roboJohnny.js
+++ roboJohnny.js
@@ -92,19 +92,19 @@ function calcAngle(dx, dy) {
 
 function flashLightAngle(angle) {
   //forward
-  if (angle >= 340 || angle <= 20) {
+  if (angle >= 350 || angle <= 10) {
     stopLight();
   }
   //backward
-  else if (angle >= 160 && angle <= 200) {
+  else if (angle >= 100 && angle <= 260) {
     backLight();
   }
   //left
-  else if (angle > 200 && angle < 340) {
+  else if (angle > 260 && angle < 350) {
     leftLight();
   }
   //right
-  else if (angle < 160 && angle > 20) {
+  else if (angle < 100 && angle > 10) {
     rightLight();
   }
 }
