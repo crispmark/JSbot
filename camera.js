9d441ef245953a17b8ebc1500025651a858ce608
crispmark <macr1324@gmail.com>
2016-03-17 15:26:25 -0400

    removed error logging

--- camera.js
+++ camera.js
@@ -8,8 +8,6 @@ function execute(port, secret) {
 
   exec(command,
       function (error, stdout, stderr) {
-          console.log('stdout: ' + stdout);
-          console.log('stderr: ' + stderr);
           if (error !== null) {
                console.log('exec error: ' + error);
                execute(port, secret);
